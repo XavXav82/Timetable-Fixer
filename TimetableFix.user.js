84e51c48fc75957ca205e092a44321d41a23d889
XavXav82 <151407766+XavXav82@users.noreply.github.com>
2024-02-22 10:48:23 +1100

    Update TimetableFix.user.js
    Added search and updates (hopefully)

--- TimetableFix.user.js
+++ TimetableFix.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name        Timetable fixer
 // @namespace   https://github.com/XavXav82/Timetable-Fixer/
-// @version     1.6.1
+// @version     1.7
 // @author      XavXav82
-// @description My plugin for timtable fixing and editing (now with colour customisation)
+// @description My plugin for timtable fixing and editing (now with colour customisation and a new search feature!)
 // @match       https://link.stleonards.vic.edu.au/timetable
 // @match       https://link.stleonards.vic.edu.au/
+// @match       https://link.stleonards.vic.edu.au/*
+// @match       https://link.stleonards.vic.edu.au/eportfolio
+// @match       https://link.stleonards.vic.edu.au/eportfolio/*
 // @grant       unsafeWindow
 // @run-at      document-start
 // @downloadURL https://github.com/XavXav82/Timetable-Fixer/raw/main/TimetableFix.user.js
@@ -13,7 +16,83 @@
 // ==/UserScript==
 
 
+//Change this to remove private study, leaving the spot blank
+let removePS = true;
+
+//Colours
+//Change your classes to the colours you want, ignore the rest
+//You can use the colours I have provided, or use your own RGB values
+//Do not delete any variables
+let spec = "rgb(255, 204, 245)"; //Pink
+let PS = "rgb(255, 255, 255)"; //White
+let meth = "rgb(204, 224, 255)"; //Blue
+let eng = "rgb(255, 245, 204)"; //Yellow
+let phys = "rgb(255,195,202)"; //Red
+let HR = "rgb(204, 204, 255)"; //Purple
+let soft = "rgb(204, 255, 204)"; //Green
+let eco = "rgb(255, 224, 204)"; //Orange
+let sys = "rgb(204, 255, 204)"; //Green
+let acc = "rgb(204, 255, 204)"; //Green
+let data = "rgb(204, 255, 204)"; //Green
+let general = "rgb(204, 255, 204)"; //Green
+let legal = "rgb(204, 255, 204)"; //Green
+let music = "rgb(204, 255, 204)"; //Green
+let IBMath = "rgb(204, 255, 204)"; //Green
+let IBMusic = "rgb(204, 255, 204)"; //Green
+let IBcomputer = "rgb(204, 255, 204)"; //Green
+
+//Any new colours must be added into the dictionaries WITH THE SAME KEY (the key is arbitrary)
+//Add a keyword into "subjects" which appears in the name of the subject
+const subjects = { methods: "Methods", english: "English", specialist: "Specialist", physics: "Physics", homeroom: "Homeroom",
+                  softwaredev: "Software", economics: "Economics", systems: "Systems", accounting: "Accounting", data: "Data",
+                  legal: "Legal", general: "General", anal:"Anal", music:"Music", IBcomp:"computer"};
+//Add the name of the variable used for colour
+const colours = { methods: meth, english: eng, specialist: spec, physics: phys, homeroom: HR, softwaredev: soft, economics: eco,
+                 systems: sys, accounting: acc, data: data, legal: legal, general: general, anal:IBMath, music:IBMusic, IBcomp:IBcomputer};
+
+
+
+var nameList;
+function searchList(){
+
+    for(let i=0;i<nameList.length;i++){
+        let name = nameList[i].split("+");
+        let h31 = document.createElement("h3");
+        let name1 = name[0].split("%20");
+        for(let j = 0;j<name1.length;j++){
+            name1[j] = name1[j][0].toUpperCase() + name1[j].substr(1);
+            //console.log(name1[j]);
+        }
+        name[0] = name1.join(" ");
+        h31.innerHTML = name[0];
+        let img1 = document.createElement("img");
+        img1.src = "https://xavxav82.github.io/stljson.github.io/fail.png";
+        let a1 = document.createElement("a");
+        a1.href = "/eportfolio/"+name[1]+"/profile";
+        a1.appendChild(img1);
+        a1.appendChild(h31);
+        let div1 = document.createElement("div");
+        div1.classList.add("card");
+        div1.classList.add("small-12");
+        console.log(div1.class);
+        div1.appendChild(a1);
+        let div2 = document.createElement("div");
+        div2.classList.add("list-item");
+        div2.appendChild(div1);
+        let li1 = document.createElement("li");
+        li1.classList.add("actions-small-1");
+        li1.appendChild(div2);
+        document.getElementsByClassName("action-list")[0].appendChild(li1);
+        //console.log(document.getElementsByClassName("action-list").length);
+    }
+}
+
+
 window.onload = function() {
+        try{
+            var searchBar = document.getElementById("search");
+            searchBar.action = "https://xavxav82.github.io/stljson.github.io/";
+        } catch{}
         //Timetable page
         if(window.location.href=="https://link.stleonards.vic.edu.au/timetable"){
             //Headings
@@ -57,6 +136,8 @@ window.onload = function() {
             }
             rows[6].remove();
 
+
+
             //Classes
             let headings = document.getElementsByTagName("th");
             headings[11].innerHTML = 'Period 1 <time class="meta">8:30am-9:50am</time>';
@@ -64,36 +145,14 @@ window.onload = function() {
             headings[13].innerHTML = 'Period 3 <time class="meta">11:35am-12:55pm</time>';
             headings[15].innerHTML = 'Period 4 <time class="meta">2:15pm-3:35pm</time>';
 
-
-            //Colours
-            //Change your classes to the colours you want, ignore the rest
-            //You can use the colours I have provided, or use your own RGB values
-            //Do not delete any variables
             let classes = document.getElementsByTagName("div");
-            let spec = "rgb(255, 204, 245)"; //Pink
-            let PS = "rgb(255, 255, 255)"; //White
-            let meth = "rgb(204, 224, 255)"; //Blue
-            let eng = "rgb(255, 245, 204)"; //Yellow
-            let phys = "rgb(255,195,202)"; //Red
-            let HR = "rgb(204, 204, 255)"; //Purple
-            let soft = "rgb(204, 255, 204)"; //Green
-            let eco = "rgb(255, 224, 204)"; //Orange
-            let sys = "rgb(204, 255, 204)"; //Green
-            let acc = "rgb(204, 255, 204)"; //Green
-            let data = "rgb(204, 255, 204)"; //Green
-            let general = "rgb(204, 255, 204)"; //Green
-            let legal = "rgb(204, 255, 204)"; //Green
-
-            const subjects = { methods: "Methods",
-                              english: "English", specialist: "Specialist", physics: "Physics", homeroom: "Homeroom", softwaredev: "Software", economics: "Economics", systems: "Systems", accounting: "Accounting", data: "Data", legal: "Legal", general: "General", };
-            const colours = { methods: meth, english: eng, specialist: spec, physics: phys, homeroom: HR, softwaredev: soft, economics: eco, systems: sys, accounting: acc, data: data, legal: legal, general: general, };
 
             //Assigning colours
             for(let i = 23;i<171;i++){
                 for (const property in subjects) {
                     if((classes[i].innerHTML).search(`${subjects[property]}`) != -1 && (classes[i].innerHTML).search("Private Study") == -1){
                         classes[i].style.backgroundColor = `${colours[property]}`;
-                    } else if((classes[i].innerHTML).search("Private Study") != -1){
+                    } else if((classes[i].innerHTML).search("Private Study") != -1 && removePS == true){
                         classes[i].remove();
                     }
                 }
@@ -104,6 +163,7 @@ window.onload = function() {
             //Remove and replace period headings
             let rows = document.getElementsByTagName("th");
             let yes = document.getElementsByTagName("td");
+            console.log(yes[1].innerHTML);
             let divs = 0;
             let a = 0
             for(let i = 1; i<10;i++){
@@ -130,6 +190,17 @@ window.onload = function() {
             periods[2].remove();
             periods[3].remove();
             periods[5].remove();
+
+            //Assigning colours
+            for(let i = 0;i<yes.length;i++){
+                for (const property in subjects) {
+                    if((yes[i].innerHTML).search(`${subjects[property]}`) != -1 && (yes[i].innerHTML).search("Private Study") == -1){
+                        yes[i].style.backgroundColor = `${colours[property]}`;
+                    } else if((yes[i].innerHTML).search("Private Study") != -1 && removePS == true){
+                        yes[i].innerHTML = "";
+                    }
+                }
+            }
 
             //Reassign location of active box from period b to a
             for(let j = 15; j<25;j++){
@@ -158,5 +229,24 @@ window.onload = function() {
             rows[17].innerHTML = 'Period 3 <time class="meta">11:35am-12:55pm</time>';
             rows[20].innerHTML = 'Period 4 <time class="meta">2:15pm-3:35pm</time>';
 
+        }else if(window.location.href.search("search")!=-1){
+
+            let em = document.getElementsByTagName("em");
+            em[0].remove();
+            const queryString = window.location.search;
+            let tempParam = (queryString.split("keyword="))[1];
+            let params = (tempParam.split("&searchval="));
+            nameList = params[0].split(",");
+            //for(let i=0;i<nameList.length;i++){
+            //    console.log(nameList[i]);
+            //}
+            //console.log(nameList);
+            let divs = document.getElementsByClassName("small-12 island");
+            let h1 = divs[0].getElementsByTagName("h1");
+            h1[0].innerHTML = "Search Results for "+params[1];
+            setTimeout(searchList, 2500);
+
         }
 };
+
+
