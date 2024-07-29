257e93032c8b2c975b34ab230afa86731db133b8
XavXav82 <151407766+XavXav82@users.noreply.github.com>
2024-07-30 08:54:28 +1000

    Update TimetableFix.user.js

--- TimetableFix.user.js
+++ TimetableFix.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Timetable fixerqq
+// @name        Timetable fixer
 // @namespace   https://github.com/XavXav82/Timetable-Fixer/
 // @version     1.10
 // @author      XavXav82
